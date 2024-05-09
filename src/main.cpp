5b1fe0d6f601ec87f47f736f9a6e9ab662cde642
Erik Johansson <cgerikj@gmail.com>
2024-05-09 12:11:19 +0200

    Downgrade glad/OpenGL to 4.4 (Minimum required for glBufferStorage)

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ void create_chunk();
 
 GLFWwindow* init_window() {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_SAMPLES, 4);
 
